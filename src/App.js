f84a32eb1ee9393b669c24e015f4b08d237428a1
DavBaghdasaryan <baghdasaryandav@gmail.com>
2020-08-29 10:57:34 +0400

    Redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
-import Todo from './Todo/index'
 
 
 class App extends Component {
+ 
+  state = {
+    count: 0
+  }
+ 
+  handleCounterUp = () => {
+    const { store } = this.props
+    store.dispatch({ type: 'INCREMENT' })
+    this.setState({count: store.getState()})
+  }
+
+  handleCounterDown = () => {
+    const { store } = this.props
+    store.dispatch({ type: 'DECREMENT' })
+    this.setState({count: store.getState()})
+  }
+
+  handleCounterCustom = () => {
+    const { store } = this.props
+    store.dispatch({ type: 'CUSTOM_NUMBER', payload: 5 })
+    this.setState({count: store.getState()})
+  }
+
+ 
   render() {
     return (
       <div>
-        <Todo />
+        <p>Count = {this.state.count}</p>
+        <button onClick={this.handleCounterUp}>Counter up</button>
+        <button onClick={this.handleCounterDown}>Counter down</button>
+        <button onClick={this.handleCounterCustom}>Another</button>
+
       </div>
       )
   }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux'
+
+
+
+const initialState = 0;
+
+function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'CUSTOM_NUMBER':
+      return state + action.payload
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App  store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
